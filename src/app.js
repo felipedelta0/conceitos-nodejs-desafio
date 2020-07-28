1b09326e0a3e3b14c61314a72329b2098392dcbc
Luis Felipe <felipedelta0@gmail.com>
2020-07-27 22:14:30 -0300

    Conclusão do código do desafio com adicionais

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,80 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRequestID (request, response, next) {
+  const { id } = request.params
+
+  if (!isUuid(id))
+    return response.status(400).send()
+
+  return next()
+}
+
+function logRequest (request, response, next) {
+  const { method, url } = request
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`
+
+  console.time(logLabel)
+
+  next()
+
+  console.timeEnd(logLabel)
+}
+
+app.use(logRequest)
+app.use('/repositories/:id', validateRequestID)
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(repository)
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+
+  if (repositoryIndex < 0)
+    return response.status(400).send()
+
+  const repository = { ...repositories[repositoryIndex], id, title, url, techs }
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+
+  if (repositoryIndex < 0)
+    return response.status(400).send()
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send()
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateRequestID, (request, response) => {
+  const { id } = request.params
+  const repositoryIndex = repositories.findIndex(repo => repo.id === id)
+
+  if (repositoryIndex < 0)
+    return response.status(400).send()
+
+  const likes = ++repositories[repositoryIndex].likes
+
+  return response.json({ likes })
 });
 
 module.exports = app;
